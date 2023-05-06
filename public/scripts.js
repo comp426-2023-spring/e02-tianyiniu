33892e95ee9fa969413b6100af1fdd1de62b5ff6
Tianyi Niu <tianyin4@email.unc.edu>
2023-05-05 22:42:32 -0400

    Fetch tests working

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,19 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function test() {
+    return response = fetch("/app/rpsls/play/paper")
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+function fetchTest() {
+    test().then(someVal => {
+        console.log("Shit");
+        console.log(someVal);
+    })
+}
