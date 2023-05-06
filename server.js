dc2872fa9ef100f88837f8e58b50cdb63346cb57
Tianyi Niu <tianyin4@email.unc.edu>
2023-05-05 21:59:40 -0400

    Added routes to server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
+
 // Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 
 import { createRequire } from 'node:module';
-import { rps, rpsls } from "./lib/rpsls.js";
 
 const require = createRequire(import.meta.url);
 
@@ -131,3 +132,49 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+import { rps, rpsls } from "./lib/rpsls.js";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK');
+})
+
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
+  });
